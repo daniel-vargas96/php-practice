c7723d64a8a649e5bbd3b33a5a8094313c21d91d
Daniel Vargas <dannyvargashb@gmail.com>
2020-11-24 22:30:55 -0800

    started php arrays

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<!-- Numeric Arrays
+
+Numeric arrays use number as access keys.
+An access key is a reference to a memory slot in
+an array variable.
+The access key is used whenever we want to read or
+assign a new value an array element. -->
+<?php
+  $variable_name[n] = value;
+?>
